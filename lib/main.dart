aa442e0f2a628f09043f527a4cf522574d8a73b9
Anshwastakenn <anshm953@gmail.com>
2024-03-29 12:57:59 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Ansh Learning Flutter"),
-          ),
-        ),
-      ),
+      home: HomePage()
     );
   }
 }
