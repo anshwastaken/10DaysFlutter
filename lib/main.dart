2f86e38101c0ad734251d58d5866276bc33a8741
Anshwastakenn <anshm953@gmail.com>
2024-03-30 00:09:13 +0530

    First day complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/home_page.dart';
+import 'package:flutter_learning/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes:{
+        "/" :(context) => LoginPage(),
+        "/login":(context) => HomePage()
+      },
     );
   }
 }
